4fef123184a9628aa2e329e9e1070e4bd4b7747f
jayesh shinde <jayeshshinde746@gmail.com>
2024-06-05 15:02:49 +0530

    location added

--- App.js
+++ App.js
@@ -2,13 +2,35 @@ import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import style from './App.Style.js';
 import HomeImage from './assets/Home.jpeg'
+import Home from './Pages/Home.js'
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { useEffect, useState } from 'react';
 export default function App() {
+  const [coordinates, setCoordinates] = useState()
+  
+  useEffect(()=>{
+    getLocation();
+  })
+  async function getLocation() {
+    const status = await requestForegroundPermissionsAsync();
+
+    if (status == "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lon: location.coords.longitude,
+      })
+    }
+    else {
+      console.log("error")
+    }
+  }
   return (
     <>
-    <ImageBackground source={HomeImage}>
-      <SafeAreaProvider>
-        <SafeAreaView style={style.container}/>
-      </SafeAreaProvider>
+      <ImageBackground source={HomeImage} imageStyle={style.img}>
+        <SafeAreaProvider>
+          <SafeAreaView style={style.container} />
+        </SafeAreaProvider>
       </ImageBackground>
     </>
   );
