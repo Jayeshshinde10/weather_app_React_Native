083790d5aa30f5b7e29ece2f3a8a01ab05770593
jayesh shinde <jayeshshinde746@gmail.com>
2024-06-06 01:31:07 +0530

    require function added

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import style from './App.Style.js';
-import HomeImage from './assets/Home.jpeg'
+import HomeImage from './assets/Home.jpeg';
 import Home from './Pages/Home/Home.js';
 import { useFonts } from 'expo-font';
-
 //import { GeoWeatherApi } from './Api/Api.js';
-//import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from 'expo-location';
 
-import { useEffect, useState } from 'react-native';
+import { useEffect, useState } from 'react';
 export default function App() {
-  const [coordinates, setCoordinates] = useState()
-  const [weather,setWeatherData] = useState()
-  const {isloaded} = useFonts({
-    "kanit-light":"./assests/kanit-Light.ttf"
-  })
+  const [coordinates, setCoordinates] = useState();
+  const [weather, setWeatherData] = useState();
+  const [ isloaded ] = useFonts({
+    'kanit-light': require('./assests/kanit-Light.ttf'),
+  });
 
-  useEffect(()=>{
+  useEffect(() => {
     getLocation();
-  },[])
-
+  }, []);
 
   // useEffect(()=>{
   //   if(coordinates){
@@ -29,44 +30,46 @@ export default function App() {
   // },[coordinates])
 
   async function getLocation() {
-        try{
-    const { status } = await requestForegroundPermissionsAsync();
+    try {
+      const { status } = await requestForegroundPermissionsAsync();
 
-    if (status == "granted") {
-      const location = await getCurrentPositionAsync();
-      setCoordinates({
-        lat: location.coords.latitude,
-        lon: location.coords.longitude,
-      })
-      console.log("coordinates are ")
-      console.log(coordinates)
+      if (status == 'granted') {
+        const location = await getCurrentPositionAsync();
+        setCoordinates({
+          lat: location.coords.latitude,
+          lon: location.coords.longitude,
+        });
+        console.log('coordinates are ');
+        console.log(coordinates);
+      } else {
+        console.log('error occured ');
+      }
+    } catch (error) {
+      console.log('error is ' + error.message);
     }
-    else {
-      console.log("error occured ")
-    }
-        }
-        catch(error){
-          console.log("error is "+error.message)
-        }
   }
 
-  async function WeatherFuncCaller(coords){
-    const weatherdata = await GeoWeatherApi.getWeatherFromApi(coords)
+  async function WeatherFuncCaller(coords) {
+    const weatherdata = await GeoWeatherApi.getWeatherFromApi(coords);
     setWeatherData(weatherdata);
   }
 
   return (
     <>
-      <ImageBackground source={HomeImage} imageStyle={style.img} style={style.container}>
+      <ImageBackground
+        source={HomeImage}
+        imageStyle={style.img}
+        style={style.container}>
         <SafeAreaProvider>
           <SafeAreaView style={style.container} />
-        {
-        coordinates && <>
-        <Text>latitude is {coordinates.lat}</Text>
-        <Text>longitude is {coordinates.lon}</Text>
-        </>
-        }
-        {isloaded && <Text style={{fontFamily:"kanit-light",fontSize:30}}>Hello world </Text>}
+          {coordinates && (
+            <>
+              <Text style={{ fontFamily: 'kanit-light', fontSize: 30 }}>
+                latitude is {coordinates.lat}
+              </Text>
+              <Text style={{fontSize:30}}>longitude is {coordinates.lon}</Text>
+            </>
+          )}
         </SafeAreaProvider>
       </ImageBackground>
     </>
